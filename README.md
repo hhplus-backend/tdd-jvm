9a7fa648881566db697f102fc5d5345bbd1d307c
ydj515 <ydj515@hanmail.net>
2024-12-15 23:46:22 +0900

    init project

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # tdd-jvm
 this is tdd-jvm
+
+## 요구사항
+
+- PATCH  `/point/{id}/charge` : 포인트를 충전한다.
+- PATCH `/point/{id}/use` : 포인트를 사용한다.
+- *GET `/point/{id}` : 포인트를 조회한다.*
+- *GET `/point/{id}/histories` : 포인트 내역을 조회한다.*
+- *잔고가 부족할 경우, 포인트 사용은 실패하여야 합니다.*
+- *동시에 여러 건의 포인트 충전, 이용 요청이 들어올 경우 순차적으로 처리되어야 합니다.*
---
+++ src/main/kotlin/io/hhplus/tdd/point/PointController.kt
@@ -0,0 +1,56 @@
+package io.hhplus.tdd.point
+
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.web.bind.annotation.*
+import java.util.Collections.emptyList
+
+@RestController
+@RequestMapping("/point")
+class PointController {
+    private val logger: Logger = LoggerFactory.getLogger(PointController::class.java)
+
+
+    /**
+     * TODO - 특정 유저의 포인트를 조회하는 기능을 작성해주세요.
+     */
+    @GetMapping("{id}")
+    fun point(
+        @PathVariable id: Long,
+    ): UserPoint {
+        logger.info("111")
+        return UserPoint(0, 0, 0)
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트 충전/이용 내역을 조회하는 기능을 작성해주세요.
+     */
+    @GetMapping("{id}/histories")
+    fun history(
+        @PathVariable id: Long,
+    ): List<PointHistory> {
+        return emptyList()
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트를 충전하는 기능을 작성해주세요.
+     */
+    @PatchMapping("{id}/charge")
+    fun charge(
+        @PathVariable id: Long,
+        @RequestBody amount: Long,
+    ): UserPoint {
+        return UserPoint(0, 0, 0)
+    }
+
+    /**
+     * TODO - 특정 유저의 포인트를 사용하는 기능을 작성해주세요.
+     */
+    @PatchMapping("{id}/use")
+    fun use(
+        @PathVariable id: Long,
+        @RequestBody amount: Long,
+    ): UserPoint {
+        return UserPoint(0, 0, 0)
+    }
+}
