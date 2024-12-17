8433ca929b37cc04c66bdd0800f4bb7ee4255da9
ydj515 <ydj515@hanmail.net>
2024-12-17 13:38:52 +0900

    feat: lock 매커니즘을 aop, annotation으로 관심사

--- src/main/kotlin/io/hhplus/tdd/LockManager.kt
+++ src/main/kotlin/io/hhplus/tdd/LockManager.kt
@@ -2,19 +2,75 @@ package io.hhplus.tdd
 
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
+import java.util.concurrent.Executors
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
 
 @Component
 class LockManager {
-    private val locks: MutableMap<Long, ReentrantLock> = ConcurrentHashMap()
+    private val locks: MutableMap<String, ReentrantLock> = ConcurrentHashMap()
 
-    fun <T> withLock(userId: Long, action: () -> T): T {
-        val lock = locks.computeIfAbsent(userId) { ReentrantLock() }
+    /**
+     * 명시적으로 락을 획득합니다.
+     * 락 획득 후 수행이 다되면 락을 해제
+     * return lockManager.withLock(key) {
+     * ...
+    }
+     */
+    fun <T> withLock(key: String, action: () -> T): T {
+        val lock = locks.computeIfAbsent(key) { ReentrantLock() }
         lock.lock()
         try {
             return action()
         } finally {
             lock.unlock()
         }
+    }
+
+    /**
+     * 명시적으로 락을 획득합니다.
+     * time 시간 동안 락 획득을 시도하며, 주어진 시간 내에 성공적으로 락을 획득하면
+     * true, 시간이 초과되면 false를 return
+     */
+    fun lock(key: String, time: Long, unit: TimeUnit): Boolean {
+        val lock = locks.computeIfAbsent(key) { ReentrantLock() }
+        return lock.tryLock(time, unit)
+    }
+
+    /**
+     * 명시적으로 락을 획득합니다.
+     * leasetime 후에 자동으로 락을 해제합니다.
+     */
+    fun lockWithLeaseTime(key: String, leaseTime: Long, unit: TimeUnit): Boolean {
+        val lock = locks.computeIfAbsent(key) { ReentrantLock() }
+
+        val lockAcquired = lock.tryLock()
+        if (lockAcquired) {
+            // leaseTime 후 락을 해제
+            Executors.newSingleThreadScheduledExecutor().schedule({
+                unlock(key)
+            }, leaseTime, unit)
+        }
+        return lockAcquired
+    }
+
+    /**
+     * 명시적으로 락을 획득합니다.
+     */
+    fun lock(key: String) {
+        val lock = locks.computeIfAbsent(key) { ReentrantLock() }
+        lock.lock()
+    }
+
+    /**
+     * 명시적으로 락을 해제합니다.
+     * 락이 존재하지 않거나, 현재 스레드가 락을 보유하고 있지 않은 경우에도 오류를 발생시키지 않습니다.
+     */
+    fun unlock(key: String) {
+        val lock = locks[key]
+        if (lock != null && lock.isHeldByCurrentThread) {
+            lock.unlock()
+        }
+        // 락이 없거나 다른 스레드가 보유 중이어도 그냥 무시
     }
 }
--- src/main/kotlin/io/hhplus/tdd/point/PointCommand.kt
+++ src/main/kotlin/io/hhplus/tdd/point/PointCommand.kt
@@ -1,6 +1,7 @@
 package io.hhplus.tdd.point
 
 import io.hhplus.tdd.LockManager
+import io.hhplus.tdd.annotation.SyncLock
 import io.hhplus.tdd.database.PointHistoryTable
 import io.hhplus.tdd.database.UserPointTable
 import org.springframework.stereotype.Component
@@ -11,25 +12,25 @@ class PointCommand(
     private val pointHistoryTable: PointHistoryTable,
     private val lockManager: LockManager
 ) {
+    @SyncLock(key = "#userId")
     fun chargePoint(userId: Long, amount: Long): UserPoint {
-        return lockManager.withLock(userId) {
-            val userPoint = userPointTable.selectById(userId)
-            val updatedUserPoint = userPoint.increasePoints(amount)
+        val userPoint = userPointTable.selectById(userId)
+        val updatedUserPoint = userPoint.increasePoints(amount)
 
-            val savedUserPoint = userPointTable.insertOrUpdate(userId, updatedUserPoint.point)
-            pointHistoryTable.insert(updatedUserPoint.id, amount, TransactionType.CHARGE, System.currentTimeMillis())
-            savedUserPoint
-        }
+        val savedUserPoint = userPointTable.insertOrUpdate(userId, updatedUserPoint.point)
+        pointHistoryTable.insert(updatedUserPoint.id, amount, TransactionType.CHARGE, System.currentTimeMillis())
+
+        return savedUserPoint
     }
 
+    @SyncLock(key = "#userId")
     fun usePoint(userId: Long, amount: Long): UserPoint {
-        return lockManager.withLock(userId) {
-            val userPoint = userPointTable.selectById(userId)
-            val updatedUserPoint = userPoint.deductPoints(amount)
+        val userPoint = userPointTable.selectById(userId)
+        val updatedUserPoint = userPoint.deductPoints(amount)
 
-            val savedUserPoint = userPointTable.insertOrUpdate(userId, updatedUserPoint.point)
-            pointHistoryTable.insert(updatedUserPoint.id, -amount, TransactionType.USE, System.currentTimeMillis())
-            savedUserPoint
-        }
+        val savedUserPoint = userPointTable.insertOrUpdate(userId, updatedUserPoint.point)
+        pointHistoryTable.insert(updatedUserPoint.id, -amount, TransactionType.USE, System.currentTimeMillis())
+
+        return savedUserPoint
     }
 }
